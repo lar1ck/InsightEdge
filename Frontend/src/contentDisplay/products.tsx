bc0e2a498442237ad04aeee588d52a794df5fbc7
lar1ck <shyakangango77@gmail.com>
2024-10-20 23:20:52 +0200

    .

--- Frontend/src/contentDisplay/products.tsx
+++ Frontend/src/contentDisplay/products.tsx
@@ -44,7 +44,7 @@ const Products = () => {
     return (
         <div className=''>
             <div className='font-semibold mt-2  flex items-center justify-between'>
-                <h1 className='text-3xl'>Products' List</h1>
+                <h1 className='text-3xl'>Products</h1>
                 <div
                     className='text-right py-1 text-white rounded-lg pr-4 pl-2 bg-custom-blue group hover:bg-custom-dblue duration-300 '>
                     <Link to="/product/new" className='bg-custom-blue group group-hover:bg-custom-dblue duration-300 flex items-center'>
